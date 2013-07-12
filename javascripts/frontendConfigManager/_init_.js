b7e2ed460f1c962a80700e7b9f7919565cb6363e
lizhong8532 <mail@lizhong.me>
2013-07-12 15:40:56 +0800

    first push

---
+++ javascripts/frontendConfigManager/_init_.js
@@ -0,0 +1,9 @@
+
+
+
+uinv.util.require([ 
+		'javascripts/frontendConfigManager/config.css',
+		'views/config/plug/Colorpicke/spectrum.css',
+		'views/config/plug/Colorpicke/spectrum.js?'+Date.parse(new Date()),
+		'javascripts/frontendConfigManager/core.js?'+Date.parse(new Date())
+]);
